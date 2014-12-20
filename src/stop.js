a20ea769688f917c48ff1e6c1d40f73554ae8cb5
justinvdm <justinvdm.0.1.0@gmail.com>
2014-12-20 15:01:30 +0200

    support disconnecting all ugens connected to a bus with stop()

--- src/stop.js
+++ src/stop.js
@@ -1,6 +1,6 @@
 wires.stop = function() {
   var all = sig.all,
-      then = sig.then,
+      map = sig.map,
       spread = sig.spread
 
   var rm = wires.utils.rm
@@ -9,13 +9,29 @@ wires.stop = function() {
   function stop(ugen, bus) {
     return vv([ugen, bus])
       (all)
-      (then, spread(function(ugen, bus) {
-        if (!bus) ugen.disconnect()
-        else ugen.disconnect(bus)
-        rm(wires.lives.store, ugen)
-        return ugen
+      (map, spread(function(obj, bus) {
+        return obj instanceof wires.gib.bus
+          ? disconnectBus(obj)
+          : disconnectUgen(obj, bus)
       }))
       ()
+  }
+
+
+  function disconnectBus(bus) {
+    var inputs = bus.inputs
+    var i = inputs.length
+    var input
+    while (i--) disconnectUgen(inputs[i].value, bus)
+    return bus
+  }
+
+
+  function disconnectUgen(ugen, bus) {
+    if (!bus) ugen.disconnect()
+    else ugen.disconnect(bus)
+    rm(wires.lives.store, ugen)
+    return ugen
   }
 
 
--- tests/stop.test.js
+++ tests/stop.test.js
@@ -32,4 +32,21 @@ describe("wires.stop", function() {
         done()
       }))
   })
+
+  it("should disconnect all ugens from a given bus", function(done) {
+    vv([bus(), sine(), sine()])
+      (all)
+      (then, spread(function(bus, ugen1, ugen2) {
+        out(ugen1, bus)
+        out(ugen2, bus)
+        bus.inputs.should.have.length(2)
+        bus.inputs[0].value.should.equal(ugen1)
+        bus.inputs[1].value.should.equal(ugen2)
+
+        stop(bus)
+        bus.inputs.should.be.empty
+
+        done()
+      }))
+  })
 })
